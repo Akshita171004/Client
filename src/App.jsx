c663e2f6e915c871130137d4bae99353585a167a
Akshita171004 <akshitakaushik17@gmail.com>
2025-06-19 10:32:28 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let res = await axios.post('http://localhost:3000/register', {
+      phoneNumber,
+      email,
+      password
+    })
+    console.log(res.data)
+    alert('User registered successfully!')
+  }
+  
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Phone Number: </label>
+          <input
+            type="text"
+            value={phoneNumber}
+            onChange={e => setPhoneNumber(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Email: </label>
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
+
